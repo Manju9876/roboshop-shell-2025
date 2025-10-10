cbbb32b1761bde452db09b0f544767b8d75a1731
Manju <manjunath960650@gmail.com>
2025-10-10 15:06:33 +0530

    new changes in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -6,7 +6,7 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-#some file need to be created
+cp roboshop.conf /etc/nginx.conf
 systemctl enable nginx
 systemctl start nginx
 systemctl status nginx
