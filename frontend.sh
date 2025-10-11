b3448729e9365892bfc04c109b6011effef0368e
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-12 00:09:08 +0530

    mongodb conf file changesconfig changes

--- frontend.sh
+++ frontend.sh
@@ -7,20 +7,27 @@ dnf module enable nginx:1.24 -y
 echo -e "\e[31m>>>>>>>>>>>>>>> install nginx <<<<<<<<<<<<<<<<\e[0m"
 dnf install nginx -y
 
+echo -e "\e[31m>>>>>>>>>>>>>>> start nginx service  <<<<<<<<<<<<<<<<\e[0m"
+systemctl enable nginx
+systemctl start nginx
+
+
 echo -e "\e[31m>>>>>>>>>>>>>>> remove Nginx data <<<<<<<<<<<<<<<<\e[0m"
-mkdir -p /usr/share/nginx/html
+#mkdir -p /usr/share/nginx/html
 rm -rf /usr/share/nginx/html/*
 
 echo -e "\e[31m>>>>>>>>>>>>>>> Download frontend code <<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-echo -e "\e[31m>>>>>>>>>>>>>>> copy nginx configuration file <<<<<<<<<<<<<<<<\e[0m"
-cp /home/ec2-user/roboshop-shell-2025/nginx.conf /etc/nginx/nginx.conf
-
 echo -e "\e[31m>>>>>>>>>>>>>>> unzip code <<<<<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[31m>>>>>>>>>>>>>>> start nginx service  <<<<<<<<<<<<<<<<\e[0m"
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[31m>>>>>>>>>>>>>>> copy nginx configuration file <<<<<<<<<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/nginx.conf /etc/nginx/nginx.conf
+
+echo -e "\e[31m>>>>>>>>>>>>>>> Restart Nginx <<<<<<<<<<<<<<<<\e[0m"
+systemctl restart nginx
+
+
+
