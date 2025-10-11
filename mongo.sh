88fb0593d3ade7ade420f6fdb86670bf9cf985b7
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-11 13:09:58 +0530

    mongodb conf file changes

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
+sed  -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
-#need to edit file and set 127.0.0.0 to 0.0.0.0
-systemctl restart mongod
+systemctl restart  mongod
