c5296ebdc8a1e28ace81a7d70f4ce7cf8c010d30
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-11 18:52:53 +0530

    mongodb conf file changes

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,25 @@
+echo -e "\e[31m>>>>>>>>>>>>>>> dsiable nginx <<<<<<<<<<<<<<<<\e[0m"
 dnf module disable nginx -y
+
+echo -e "\e[31m>>>>>>>>>>>>>>> Enable nginx version 1.24 <<<<<<<<<<<<<<<<\e[0m"
 dnf module enable nginx:1.24 -y
+
+echo -e "\e[31m>>>>>>>>>>>>>>> install nginx <<<<<<<<<<<<<<<<\e[0m"
 dnf install nginx -y
+
+echo -e "\e[31m>>>>>>>>>>>>>>> remove Nginx data <<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[31m>>>>>>>>>>>>>>> Download frontend code <<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-cp nginx.conf /etc/nginx/nginx.conf
+
+echo -e "\e[31m>>>>>>>>>>>>>>> dsiable nginx <<<<<<<<<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/nginx.conf /etc/nginx/nginx.conf
+
+echo -e "\e[31m>>>>>>>>>>>>>>> unzip code <<<<<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[31m>>>>>>>>>>>>>>> start nginx service  <<<<<<<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl restart nginx
