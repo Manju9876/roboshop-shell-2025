73b80b63b45879313939e602ce0e954c9d0e5a44
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-12 21:05:27 +0530

    code changes

--- redis.sh
+++ redis.sh
@@ -1,6 +1,12 @@
+echo -e "\e[31m>>>>>>>>>>>>>> Disable redis <<<<<<<<<<<<<<<<\e[0m"
 dnf module disable redis -y
+
+echo -e "\e[31m>>>>>>>>>>>>>> Enable redis version 7 <<<<<<<<<<<<<<<<\e[0m"
 dnf module enable redis:7 -y
+
+echo -e "\e[31m>>>>>>>>>>>>>> Install redis <<<<<<<<<<<<<<<<\e[0m"
 dnf install redis -y
+
 #need to edit conf file
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
