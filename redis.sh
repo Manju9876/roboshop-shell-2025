caeb0f03e9845b5a27127edebfbf6320531f2999
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-12 21:06:37 +0530

    code changes

--- redis.sh
+++ redis.sh
@@ -7,6 +7,9 @@ dnf module enable redis:7 -y
 echo -e "\e[31m>>>>>>>>>>>>>> Install redis <<<<<<<<<<<<<<<<\e[0m"
 dnf install redis -y
 
+echo -e "\e[31m>>>>>>>>>>>>>> change localhost to 0.0.0.0 in redis.conf <<<<<<<<<<<<<<<<\e[0m"
 #need to edit conf file
+
+echo -e "\e[31m>>>>>>>>>>>>>> enable and start redis <<<<<<<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl restart redis
