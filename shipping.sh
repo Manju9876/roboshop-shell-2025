091542c64e8dc1e0f596446bdbac8739f4a5c5f9
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-13 22:06:05 +0530

    changes

--- shipping.sh
+++ shipping.sh
@@ -13,9 +13,6 @@ mkdir /app
 echo -e "\e[31m>>>>>>>>>>>> Download App code <<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
 
-echo -e "\e[31m>>>>>>>>>>>> Copy shipping service file <<<<<<<<<\e[0m"
-cp /home/ec2-user/roboshop-shell-2025/shipping.service /etc/systemd/system/shipping.service
-
 echo -e "\e[31m>>>>>>>>>>>> change to app directory and unzip code <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
@@ -27,12 +24,15 @@ mvn clean package
 echo -e "\e[31m>>>>>>>>>>>> move  <<<<<<<<<\e[0m"
 mv target/shipping-1.0.jar shipping.jar
 
+echo -e "\e[31m>>>>>>>>>>>> Copy shipping service file <<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/shipping.service /etc/systemd/system/shipping.service
+
 echo -e "\e[31m>>>>>>>>>>>> Daemon reload <<<<<<<<<\e[0m"
 systemctl daemon-reload
 
-echo -e "\e[31m>>>>>>>>>>>> enable and start shipping service <<<<<<<<<\e[0m"
-systemctl enable shipping
-systemctl start shipping
+#echo -e "\e[31m>>>>>>>>>>>> enable and start shipping service <<<<<<<<<\e[0m"
+#systemctl enable shipping
+#systemctl restart shipping
 
 echo -e "\e[31m>>>>>>>>>>>> Install mysql <<<<<<<<<\e[0m"
 dnf install mysql -y
@@ -43,4 +43,5 @@ mysql -h mysql.devopsbymanju.shop -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h mysql.devopsbymanju.shop-uroot -pRoboShop@1 < /app/db/master-data.sql
 
 echo -e "\e[31m>>>>>>>>>>>> restart shipping service <<<<<<<<<\e[0m"
+systemctl enable shipping
 systemctl restart shipping
