4e8dcd17cbbde2ff4aca2360259d85923b626346
Manju <manjunath960650@gmail.com>
2025-10-13 16:16:05 +0530

    creation of mongodb

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,9 @@
+echo -e "\e[31m>>>>>>>>>>>>>>>> install mysql <<<<<<<<<<<<<\e[0m"
 dnf install mysql-server -y
+
+echo -e "\e[31m>>>>>>>>>>>>>>>> enable and start mysql <<<<<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-#need to change the default  root passowrd
+
+echo -e "\e[31m>>>>>>>>>>>>>>>> change default password to RoboShop@1 <<<<<<<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,36 @@
+echo -e "\e[31m>>>>>>>>>>>> Install maven <<<<<<<<<\e[0m"
 dnf install maven -y
+
+echo -e "\e[31m>>>>>>>>>>>> create roboshop user <<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[31m>>>>>>>>>>>> create app directory <<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[31m>>>>>>>>>>>> Download App code <<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
-cp shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[31m>>>>>>>>>>>> Copy shipping service file <<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[31m>>>>>>>>>>>> change to app directory and unzip code <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
+
+echo -e "\e[31m>>>>>>>>>>>> install maven dependencies <<<<<<<<<\e[0m"
 cd /app
 mvn clean package
-mv target/shipping-1.0.jar shipping.jar
 
+echo -e "\e[31m>>>>>>>>>>>> move  <<<<<<<<<\e[0m"
+mv /home/ec2-user/roboshop-shell-2025/app/target/shipping-1.0.jar shipping.jar
+
+echo -e "\e[31m>>>>>>>>>>>> Daemon reload <<<<<<<<<\e[0m"
 systemctl daemon-reload
+
+echo -e "\e[31m>>>>>>>>>>>> enable and start shipping service <<<<<<<<<\e[0m"
 systemctl enable shipping
-systemctl start shipping
+systemctl restart shipping
 
 dnf install mysql -y
 mysql -h mysql.devopsbymanju.shop -uroot -pRoboShop@1 < /app/db/schema.sql
