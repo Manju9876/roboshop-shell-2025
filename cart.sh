f2ce9837f9b0ef6e7f7ebe50ea422cc8d8ab91bd
Manju <manjunath960650@gmail.com>
2025-10-13 14:46:12 +0530

    creation of mongodb

--- cart.sh
+++ cart.sh
@@ -1,14 +1,35 @@
+echo -e "\e[31m>>>>>>>>>>>>> Disable nodejs <<<<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
+
+echo -e "\e[31m>>>>>>>>>>>>> Enable nodejs version 20 <<<<<<<<<<<<\e[0m"
 dnf module enable nodejs:20 -y
+
+echo -e "\e[31m>>>>>>>>>>>>> Install nodejs <<<<<<<<<<<<\e[0m"
 dnf install nodejs -y
+
+echo -e "\e[31m>>>>>>>>>>>>> create user <<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[31m>>>>>>>>>>>>> Create a directory <<<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[31m>>>>>>>>>>>>> Download application code <<<<<<<<<<<<\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[31m>>>>>>>>>>>>> Copy systemd file <<<<<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[31m>>>>>>>>>>>>> unzip application code in /app <<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/cart.zip
-cd /app
+
+echo -e "\e[31m>>>>>>>>>>>>> install dependencies  <<<<<<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[31m>>>>>>>>>>>>> Daemon reload <<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
+
+echo -e "\e[31m>>>>>>>>>>>>>enable and restart catalogue service <<<<<<<<<<<<\e[0m"
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
