1e5f55d69b8ac579e9c38fc2ed134651fcbb19dd
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-13 23:10:17 +0530

    changes

--- payment.sh
+++ payment.sh
@@ -1,12 +1,29 @@
+echo -e "\e[31m>>>>>>>>>>>>> Install Python <<<<<<<<<<<<<<<\e[0m"
 dnf install python3 gcc python3-devel -y
+
+echo -e "\e[31m>>>>>>>>>>>>> create app user  <<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[31m>>>>>>>>>>>>> create a directory <<<<<<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[31m>>>>>>>>>>>>> download app content <<<<<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cp payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[31m>>>>>>>>>>>>> copy payment service file <<<<<<<<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[31m>>>>>>>>>>>>> navigate to /app and unzip app content <<<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/payment.zip
-cd /app
+
+echo -e "\e[31m>>>>>>>>>>>>> download python dependencies <<<<<<<<<<<<<<<\e[0m"
 pip3 install -r requirements.txt
+
+echo -e "\e[31m>>>>>>>>>>>>> reload systemd file <<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
+
+echo -e "\e[31m>>>>>>>>>>>>> enable and restart payment service <<<<<<<<<<<<<<<\e[0m"
 systemctl enable payment
-systemctl start payment
+systemctl restart payment
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,15 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+echo -e "\e[31m>>>>>>>>>>>>> copy repo file <<<<<<<<<<<<<<<\e[0m"
+cp /home/ec2-user/roboshop-shell-2025/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+
+echo -e "\e[31m>>>>>>>>>>>>> Install rabbitMQ <<<<<<<<<<<<<<<\e[0m"
 dnf install rabbitmq-server -y
+
+echo -e "\e[31m>>>>>>>>>>>>> add user and password <<<<<<<<<<<<<<<\e[0m"
+rabbitmqctl add_user roboshop roboshop123
+
+echo -e "\e[31m>>>>>>>>>>>>> set permissions <<<<<<<<<<<<<<<\e[0m"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+echo -e "\e[31m>>>>>>>>>>>>> enable and start rabbitMQ <<<<<<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
