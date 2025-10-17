11186ca5054fe4c1b58523f47eddbb6f3048a90f
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-17 21:40:14 +0530

    script path

--- cart.sh
+++ cart.sh
@@ -1,39 +1,7 @@
-script_path=$(dirname $0)
+script=$(realpath $0)
+script_path=$(dirname "${script}")
 source ${script_path}/common.sh
 
+component=cart
 
-echo -e "\e[31m>>>>>>>>>>>>> Disable nodejs <<<<<<<<<<<<\e[0m"
-dnf module disable nodejs -y
-
-echo -e "\e[31m>>>>>>>>>>>>> Enable nodejs version 20 <<<<<<<<<<<<\e[0m"
-dnf module enable nodejs:20 -y
-
-echo -e "\e[31m>>>>>>>>>>>>> Install nodejs <<<<<<<<<<<<\e[0m"
-dnf install nodejs -y
-
-echo -e "\e[31m>>>>>>>>>>>>> create user <<<<<<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m>>>>>>>>>>>>> Create a directory <<<<<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m>>>>>>>>>>>>> Download application code <<<<<<<<<<<<\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-
-echo -e "\e[31m>>>>>>>>>>>>> Copy systemd file <<<<<<<<<<<<\e[0m"
-cp ${script_path}/cart.service /etc/systemd/system/cart.service
-
-echo -e "\e[31m>>>>>>>>>>>>> unzip application code in /app <<<<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/cart.zip
-
-echo -e "\e[31m>>>>>>>>>>>>> install dependencies  <<<<<<<<<<<<\e[0m"
-npm install
-
-echo -e "\e[31m>>>>>>>>>>>>> Daemon reload <<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-
-echo -e "\e[31m>>>>>>>>>>>>>enable and restart cart service <<<<<<<<<<<<\e[0m"
-systemctl enable cart
-systemctl restart cart
+func_nodejs
