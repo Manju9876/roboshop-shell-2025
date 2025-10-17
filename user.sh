fd45d861a72c8f5f1f71c63b34b3ca72167db3a4
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-17 21:34:58 +0530

    script path

--- user.sh
+++ user.sh
@@ -1,37 +1,8 @@
-script_path=$(dirname $0)
+script=$(realpath $0)
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+component=user
 
-echo -e "\e[31m>>>>>>>>>>>>> disable Node js <<<<<<<<<<<<<<\e[0m"
-dnf module disable nodejs -y
+func_nodejs
 
-echo -e "\e[31m>>>>>>>>>>>>> enable Node js version 20 <<<<<<<<<<<<<<\e[0m"
-dnf module enable nodejs:20 -y
-
-echo -e "\e[31m>>>>>>>>>>>>> Insatall Node js <<<<<<<<<<<<<<\e[0m"
-dnf install nodejs -y
-
-echo -e "\e[31m>>>>>>>>>>>>> Add roboshop user <<<<<<<<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m>>>>>>>>>>>>> Create a directory  <<<<<<<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m>>>>>>>>>>>>> Download application code <<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-
-echo -e "\e[31m>>>>>>>>>>>>> copy user.service file  <<<<<<<<<<<<<<\e[0m"
-cp ${script_path}/user.service /etc/systemd/system/user.service
-
-echo -e "\e[31m>>>>>>>>>>>>> move to /app and unzip content <<<<<<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/user.zip
-
-echo -e "\e[31m>>>>>>>>>>>>> install Node js dependencies <<<<<<<<<<<<<<\e[0m"
-npm install
-
-echo -e "\e[31m>>>>>>>>>>>>> reload service, enable and start nodejs <<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable user.service
-systemctl start user.service
