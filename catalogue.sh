321a39cc0433d1dd5f7347f5753384bbd378a5bd
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-17 21:10:20 +0530

    script path

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,10 @@
-script_path=$(dirname $(realpath "$0"))
+#script_path=$(dirname $(realpath "$0"))
 #script_path=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 
 #script_path=$(dirname $(realpath $0))
 
-#script=$(realpath $0)
-#script_path=$(dirname "$script")
+script=$(realpath $0)
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 echo ${cript_path}
 exit
