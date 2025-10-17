8cd1cef83cb7a5de1ab36e1e814e10f67bfaab01
Manju <manjunath960650@gmail.com>
2025-10-17 16:17:41 +0530

    script path changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,9 @@
-script=$(realpath $0)
-script_path=$(dirname "$script")
+script_path=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+#script_path=$(dirname $(realpath $0))
+
+#script=$(realpath $0)
+#script_path=$(dirname "$script")
 source ${script_path}/common.sh
 echo script_path
 exit
