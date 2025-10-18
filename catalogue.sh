8cf1dcf08c668b086c595540a469480b7f75a40a
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-18 23:49:07 +0530

    conditions practice

--- catalogue.sh
+++ catalogue.sh
@@ -2,12 +2,7 @@ script=$(realpath $0)
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-#script_path=$(dirname $(realpath "$0"))
-#script_path=$(dirname $(realpath "${BASH_SOURCE[0]}"))
-
-#script_path=$(dirname $(realpath $0))
-
 component=catalogue
-
-func_nodejs
 schema_setup=mongo
+func_nodejs
+
