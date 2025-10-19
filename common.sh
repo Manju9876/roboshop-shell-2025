3f3548b38dd23ba469d8ef6e9370cf669f9b4400
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-20 01:31:20 +0530

    conditions practice

--- common.sh
+++ common.sh
@@ -12,75 +12,91 @@ func_status_check(){
       echo -e "\e[32mSUCCESS\e[0m"
     else
       echo -e "\e[31mFAILURE\e[31m"
+      exit 1
     fi
 }
 func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
-  func_print_head "Copy mongod repo from mongo.repo"
-  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    func_print_head "Copy mongod repo from mongo.repo"
+      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+      func_status_check $?
 
-  func_print_head "install mongodb"
-  dnf install mongodb-mongosh -y
+    func_print_head "install mongodb"
+      dnf install mongodb-mongosh -y
+      func_status_check $?
 
-  func_print_head "connect to schema"
-  mongosh --host mongodb-dev.devopsbymanju.shop </app/db/master-data.js
+    func_print_head "connect to schema"
+      mongosh --host mongodb-dev.devopsbymanju.shop </app/db/master-data.js
+      func_status_check $?
   fi
 
   if [ "$schema_setup" == "mysql" ]; then
     func_print_head "Install Mysql Client"
-    dnf install mysql -y
+      dnf install mysql -y
 
     func_print_head "connect schemas to the root and with password"
-    mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/schema.sql
-    mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/app-user.sql
-    mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/master-data.sql
+      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/schema.sql
+      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/app-user.sql
+      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/master-data.sql
+      func_status_check $?
   fi
 }
 
 func_app_prereq(){
    func_print_head "create Application  user"
-    useradd ${app_user}
+     useradd ${app_user}
+     func_status_check $?
 
    func_print_head "delete any existing /app dir and create app directory"
-    rm -rf /app
-    mkdir /app
+     rm -rf /app
+     mkdir /app
+     func_status_check $?
 
    func_print_head "Download Application code"
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+     func_status_check $?
 
    func_print_head "Change to /app directory and unzip code"
-    cd /app
-    unzip /tmp/${component}.zip
+     cd /app
+     unzip /tmp/${component}.zip
+     func_status_check $?
 
 }
 
 func_systemd_setup(){
    func_print_head "Copy service file"
-    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+     func_status_check $?
 
    func_print_head "Daemon reload"
       systemctl daemon-reload
+      func_status_check $?
 
    func_print_head "Start ${component} service"
       systemctl enable ${component}
       systemctl restart ${component}
+      func_status_check $?
 
 }
 
 func_nodejs(){
   func_print_head "disable Node js"
   dnf module disable nodejs -y
+  func_status_check $?
 
   func_print_head "enable Node js version 20"
   dnf module enable nodejs:20 -y
+  func_status_check $?
 
   func_print_head "install Node js"
   dnf install nodejs -y
+  func_status_check $?
 
   func_app_prereq
 
   func_print_head "install Node js dependencies"
   npm install
+  func_status_check $?
 
   func_schema_setup
 
@@ -97,13 +113,12 @@ func_java(){
 
  func_print_head "Download maven dependencies"
   cd /app
-  func_status_check $?
   mvn clean package
   func_status_check $?
 
  func_print_head "Moving shipping.jar file from target dir"
   mv target/${component}-1.0.jar ${component}.jar
-  func_status_check $?
+ func_status_check $?
  func_schema_setup
 
  func_systemd_setup
