88e578ab314a438b353be8fe659784826e50db98
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-20 11:31:42 +0530

    redirectors

--- common.sh
+++ common.sh
@@ -45,7 +45,7 @@ func_schema_setup(){
 
 func_app_prereq(){
    func_print_head "create Application  user"
-     useradd ${app_user} $>/tmp/roboshop.log
+     useradd ${app_user} &>/tmp/roboshop.log
      func_status_check $?
 
    func_print_head "delete any existing /app dir and create app directory"
@@ -107,7 +107,7 @@ func_nodejs(){
 
 func_java(){
  func_print_head "Install maven"
-  dnf install maven -y >/tmp/roboshop.log
+  dnf install maven -y $>/tmp/roboshop.log
   func_status_check $?
 
  func_app_prereq
