abaf446725408e64c1c29573f7f5942c7eb9b48a
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-10-20 12:12:02 +0530

    redirectors

--- common.sh
+++ common.sh
@@ -25,26 +25,26 @@ func_schema_setup(){
 
   if [ "$schema_setup" == "mongo" ]; then
     func_print_head "Copy mongod repo from mongo.repo"
-      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo $>>${log_file}
+      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
       func_status_check $?
 
     func_print_head "install mongodb"
-      dnf install mongodb-mongosh -y $>>${log_file}
+      dnf install mongodb-mongosh -y &>>${log_file}
       func_status_check $?
 
     func_print_head "connect to schema"
-      mongosh --host mongodb-dev.devopsbymanju.shop </app/db/master-data.js $>>${log_file}
+      mongosh --host mongodb-dev.devopsbymanju.shop </app/db/master-data.js &>>${log_file}
       func_status_check $?
   fi
 
   if [ "$schema_setup" == "mysql" ]; then
     func_print_head "Install Mysql Client"
-      dnf install mysql -y $>>${log_file}
+      dnf install mysql -y &>>${log_file}
 
     func_print_head "connect schemas to the root and with password"
-      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/schema.sql $>>${log_file}
-      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/app-user.sql $>>${log_file}
-      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/master-data.sql $>>${log_file}
+      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/schema.sql &>>${log_file}
+      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/app-user.sql &>>${log_file}
+      mysql -h mysql-dev.devopsbymanju.shop -uroot -p${mysql_root_password} < /app/db/master-data.sql &>>${log_file}
       func_status_check $?
   fi
 }
@@ -59,8 +59,8 @@ func_app_prereq(){
      func_status_check $?
 
    func_print_head "delete any existing /app dir and create app directory"
-     rm -rf /app $>>${log_file}
-     mkdir /app $>>${log_file}
+     rm -rf /app &>>${log_file}
+     mkdir /app &>>${log_file}
      func_status_check $?
 
    func_print_head "Download Application code"
@@ -68,8 +68,8 @@ func_app_prereq(){
      func_status_check $?
 
    func_print_head "Change to /app directory and unzip code"
-     cd /app $>>${log_file}
-     unzip /tmp/${component}.zip $>>${log_file}
+     cd /app &>>${log_file}
+     unzip /tmp/${component}.zip &>>${log_file}
      func_status_check $?
 
 }
@@ -77,16 +77,16 @@ func_app_prereq(){
 func_systemd_setup(){
 
    func_print_head "Copy service file"
-     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service $>>${log_file}
+     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
      func_status_check $?
 
    func_print_head "Daemon reload"
-      systemctl daemon-reload $>>${log_file}
+      systemctl daemon-reload &>>${log_file}
       func_status_check $?
 
    func_print_head "Start ${component} service"
-      systemctl enable ${component} $>>${log_file}
-      systemctl restart ${component} $>>${log_file}
+      systemctl enable ${component} &>>${log_file}
+      systemctl restart ${component} &>>${log_file}
       func_status_check $?
 
 }
@@ -94,21 +94,21 @@ func_systemd_setup(){
 func_nodejs(){
 
   func_print_head "disable Node js"
-  dnf module disable nodejs -y $>>${log_file}
+  dnf module disable nodejs -y &>>${log_file}
   func_status_check $?
 
   func_print_head "enable Node js version 20"
-  dnf module enable nodejs:20 -y $>>${log_file}
+  dnf module enable nodejs:20 -y &>>${log_file}
   func_status_check $?
 
   func_print_head "install Node js"
-  dnf install nodejs -y $>>${log_file}
+  dnf install nodejs -y &>>${log_file}
   func_status_check $?
 
   func_app_prereq
 
   func_print_head "install Node js dependencies"
-  npm install $>>${log_file}
+  npm install &>>${log_file}
   func_status_check $?
 
   func_schema_setup
@@ -126,12 +126,12 @@ func_java(){
  func_app_prereq
 
  func_print_head "Download maven dependencies"
-  cd /app $>>${log_file}
-  mvn clean package $>>${log_file}
+  cd /app &>>${log_file}
+  mvn clean package &>>${log_file}
   func_status_check $?
 
  func_print_head "Moving shipping.jar file from target dir"
-  mv target/${component}-1.0.jar ${component}.jar $>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
  func_status_check $?
 
  func_schema_setup
