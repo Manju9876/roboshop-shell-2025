96c5524f3d39fdb406a16a716e615a3fa19b9f05
Manju <manjunath960650@gmail.com>
2025-10-21 12:33:02 +0530

    redis update code

--- dispatch.sh
+++ dispatch.sh
@@ -4,7 +4,7 @@ source ${script_path}/common.sh
 rabbitmq_app_username=$1
 rabbitmq_app_users_password=$2
 
-if [ -z "${rabbitmq_app_username}" ] || [ "${rabbitmq_app_users_password}" ]; then
+if [ -z "${rabbitmq_app_username}" ] || [ -z "${rabbitmq_app_users_password}" ]; then
    echo -e "\e[31m❌ Input missing!\e[0m"
    echo -e "\e[31mUsage: sudo bash script-name.sh <rabbitmq_app_username> <rabbitmq_app_users_password>\e[0m"
    exit 1
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -4,8 +4,8 @@ rabbitmq_app_username=$1
 rabbitmq_app_users_password=$2
 
 if [ -z "${rabbitmq_app_username}" ] || [ -z "${rabbitmq_app_users_password}" ]; then
-   echo "❌ Input missing!"
-   echo "Usage: sudo bash script-name.sh <rabbitmq_app_username> <rabbitmq_app_users_password>"
+   echo -e "\e[31m❌ Input missing!\e[0m"
+   echo -e "\e[31mUsage: sudo bash script-name.sh <rabbitmq_app_username> <rabbitmq_app_users_password>\e[0m"
    exit 1
 fi
 
