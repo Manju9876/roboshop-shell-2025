f5167f876a5711759c14a3c885fba247f67bcc38
Manju <manjunath960650@gmail.com>
2025-10-21 12:24:33 +0530

    redis update code

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -17,6 +17,10 @@ func_print_head "Install rabbitMQ"
 dnf install rabbitmq-server -y &>>${log_file}
   func_status_check $?
 
+func_print_head "enable and start rabbitMQ"
+  systemctl enable rabbitmq-server &>>${log_file}
+  func_status_check $?systemctl restart rabbitmq-server &>>${log_file}
+
 func_print_head "add user and password"
   rabbitmqctl add_user ${rabbitmq_app_username} ${rabbitmq_app_users_password} &>>${log_file}
   func_status_check $?
@@ -24,7 +28,3 @@ func_print_head "add user and password"
 func_print_head "set permissions"
   rabbitmqctl set_permissions -p / ${rabbitmq_app_username} ".*" ".*" ".*" &>>${log_file}
   func_status_check $?
-
-func_print_head "enable and start rabbitMQ"
-  systemctl enable rabbitmq-server &>>${log_file}
-  func_status_check $?systemctl restart rabbitmq-server &>>${log_file}
